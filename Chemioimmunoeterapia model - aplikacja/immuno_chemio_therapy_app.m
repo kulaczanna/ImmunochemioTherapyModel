a90118662e6a178793863c90bb6b327705f40471
Anna Kulacz <annakulacz@yahoo.com>
2020-01-03 21:41:40 +0100

    app v1

---
+++ Chemioimmunoeterapia model - aplikacja/immuno_chemio_therapy_app.m
@@ -0,0 +1,61 @@
+clc; clear all
+close all
+
+n = 2; % wybór przypadku - 1 st³umienie nowotworu, 2 wzrost nowotworu
+
+switch n
+    case 1
+        T_0 = 1e6;% tumor cells population T(t)
+        N_0 = 1e5;% natural killer cells N(t)
+        L_0 = 1e2;% CD8+T cells L(t)
+        C_0 = 6e10;% cell C(t)
+        I_0 = 0;% concentration of IL-2 I(t)
+        I_alfa_0 = 0;% concentration of INF-a Ia(t)
+        M_0 = 0;% concentration of chemotherapy drug M(t)
+    case 2
+        T_0 = 1e6;% tumor cells population T(t)
+        N_0 = 1e3;% natural killer cells N(t)
+        L_0 = 1e1;% CD8+T cells L(t)
+        C_0 = 6e8;% cell C(t)
+        I_0 = 0;% concentration of IL-2 I(t)
+        I_alfa_0 = 0;% concentration of INF-a Ia(t)
+        M_0 = 0;% concentration of chemotherapy drug M(t)
+end
+
+% I_alfa = I_alfa_0;
+% x = [T_0; N_0; L_0; C_0; M_0; I_0; I_alfa_0; I_alfa];
+
+x = [T_0; N_0; L_0; C_0; M_0; I_0; I_alfa_0]; % parametry wejœciowe  uk³adu równañ
+
+[t,y] = ode45(@model_immuno_chemio_therapy, [0 120], x); % rozwi¹zanie uk³adu równañ ró¿niczkowych
+
+% wyœwietlenie wykresów
+figure
+plot(t, y(:,1), 'b');
+legend('Tumor cells');
+figure
+plot(t, y(:,2), 'g');
+legend('Natural killer cells');
+figure
+plot(t, y(:,3), 'r');
+legend('CD8+ T cells');
+figure
+plot(t, y(:,4), 'c');
+legend('Circulating lymphocytes');
+
+% % legend('T');
+% hold on
+% % subplot(2,2,2)
+% plot(t, y(:,2), 'g');
+% % legend('N');
+% hold on
+% % subplot(2,2,3)
+% plot(t, y(:,3), 'r');
+% % legend('L');
+% hold on
+% subplot(2,2,4)
+% plot(t, y(:,4), 'c');
+% legend('C');
+% legend({'T'; 'N'; 'L'; 'C'; 'I'; 'Ialfa'; 'M'})
+
+
