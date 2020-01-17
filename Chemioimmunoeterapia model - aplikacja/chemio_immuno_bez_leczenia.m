435dfd16715b9c23c75956640a5c1340edcd05b2
Anna Kulacz <annakulacz@yahoo.com>
2020-01-17 13:47:35 +0100

    utworzenie plików - modeli - de Pilisa, Isaevy, ogólnego w wersji bez leczenia i  zleczeniem

--- Chemioimmunoeterapia model - aplikacja/chemio_immuno_bez_leczenia.m
+++ Chemioimmunoeterapia model - aplikacja/chemio_immuno_bez_leczenia.m
@@ -4,19 +4,11 @@ K_T = 0;
 K_N = 0;
 K_L = 0;
 K_C = 0;
-v_L = 0;
-v_M = 0;
-v_I = 0;
-v_alfa = 0;
 
 T = x(1);
 N = x(2);
 L = x(3);
 C = x(4);
-M = x(5);
-I = x(6);
-I_alfa = x(7);
-% I_alfa0 = x(8);
 
 % ustawienie parametrów
 a = 4.31e-1;
@@ -48,25 +40,18 @@ g_prim = 1.7;
 j_prim = 3.3e-9;
 k_prim = 1.8e-8;
 
-% D = d * (((L / T)^l) / (s + ((L / T)^l))); % tumor inactivation by CD8+T
 D = count_D(d, L, T, s, l);
 % c_prim = c_CTL * (2 - (exp((-I_alfa) / I_alfa0))); % I_alfa, CD8T, tumor interact - inactivation
 c_prim = c_CTL;
 
 % równania modelu
-dTdt = (a * T *(1 - (b * T))) - (c * N * T) - (D * T) - (K_T * (1 - (exp(-M))) * T) - (c_prim * T * L);
-dNdt = (e * C) - (f * N) + (g * ((T^2) / (h + (T^2))) * N) - (p * N * T) - (K_N * (1 - exp(-M)) * N);
-dLdt = ((-m) * L) + (jj * ((D^2 * T^2) / (k + (D^2 * T^2))) * L) - (q * L * T) + (((r1 * N) + (r2 * C)) * T) - (u * N * (L^2)) - (K_L * (1 - exp(-M)) * L) + ((p_i * L * I) / (g_i)) + v_L;
-dCdt = alfa - (beta * C) - (K_C * (1 - exp(-M)) * C);
-dMdt = ((-gamma) * M) + v_M;
-dIdt = ((-mi_i) * L) - (j_prim * L * I) - (k_prim * T * I) + v_I;
-dI_alfadt = v_alfa - (g * I_alfa);
-
-rownania = [dTdt; dNdt; dLdt; dCdt; dMdt; 0; 0]; 
-% rownania = [dTdt; dNdt; dLdt; dCdt; dMdt; dIdt; dI_alfadt]; % wektor równañ
-
+dTdt = (a * T *(1 - (b * T))) - (c * N * T) - (D * T);
+dNdt = (e * C) - (f * N) + (g * ((T^2) / (h + (T^2))) * N) - (p * N * T);
+dLdt = ((-m) * L) + (jj * ((D^2 * T^2) / (k + (D^2 * T^2))) * L) - (q * L * T) + (((r1 * N) + (r2 * C)) * T) - (u * N * (L^2));
+dCdt = alfa - (beta * C);
+ 
+rownania = [dTdt; dNdt; dLdt; dCdt]; % bez sk³adowych leczenia
 end
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -75,18 +60,18 @@ end
 % NK_growth = e * C - f * N;
 % CD8T_decrease = (-m) * L;
 % lymphocytes_generated = alfa - (beta * C);
-% decay_of_chemioth_drug = (-gamma) * M;
-% exp_decay_of_IL2 = (-mi_i) * L - (j_prim * L * I) - (k_prim * T * I);
-% exp_decay_of_INF_alfa = (-g) * I_alfa;
+% decay_of_chemioth_drug = (-gamma) * M; -------------------
+% exp_decay_of_IL2 = (-mi_i) * L - (j_prim * L * I) - (k_prim * T * I); ---
+% exp_decay_of_INF_alfa = (-g) * I_alfa;  ! ------------------
 % 
 % % fractional cell kill terms
 % tumor_NK_interact = (- c) * N * T;
 % tumor_inactivation_by_CD8T = (-D) * T;
-% chemio_on_tumor = K_T * (1 - (exp(-M))) * T;
-% chemio_on_NK = K_N * (1 - exp(-M)) * N;
-% chemio_on_CD8T = K_L * (1 - exp(-M)) * L;
-% chemio_on_lymphocytes = K_C * (1 - exp(-M)) * C;
-% IL2_stimmulates_CD8T = ((p_i * L * I) / g_i);
+% chemio_on_tumor = K_T * (1 - (exp(-M))) * T; ----------
+% chemio_on_NK = K_N * (1 - exp(-M)) * N; ---------------
+% chemio_on_CD8T = K_L * (1 - exp(-M)) * L; -------------
+% chemio_on_lymphocytes = K_C * (1 - exp(-M)) * C; ------
+% IL2_stimmulates_CD8T = ((p_i * L * I) / g_i); ---------
 % 
 % % recruitment terms
 % recr_NK = g * ((T^2) / (h + (T^2))) * N;
@@ -94,7 +79,7 @@ end
 % tumor_NK_CD8T_impact = ((r1 * N) + (r2 * C)) * T;
 % 
 % % inactivation terms
-% I_alfa_CD8T_tumor_inactiv = c_prim * T * L;
+% I_alfa_CD8T_tumor_inactiv = c_prim * T * L; -----------
 % NK_inactiv_after_interact_with_tumor = (-p) * N * T;
 % CD8T_inactiv_after_interact_with_tumor = (-q) * L * T;
 % NK_regulation_of_CD8T = - u * N * (L^2);
